74601254094119412e1ff7bb766a6dcd4f98b67b
MeiXue16 <76053211+MeiXue16@users.noreply.github.com>
2023-08-18 15:26:05 +0200

    function fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,21 +25,23 @@ fruits_to_show= my_fruit_list.loc[fruit_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+  
 
-
+# new section to display fruityvice response
 streamlit.header("Fruityvice Fruit Advice!")
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# streamlit.text(fruityvice_response.json())
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    back_from_function= get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)  
+except URLError as e:
+  streamlit.error()
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
